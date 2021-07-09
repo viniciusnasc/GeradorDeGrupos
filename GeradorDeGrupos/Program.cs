940551df3fef3123aef2b1468f78ab9c8caca898
viniciusnasc <vini.souza00@gmail.com>
2021-07-09 19:03:03 -0300

    Arrumando regras

--- GeradorDeGrupos/Program.cs
+++ GeradorDeGrupos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace GeradorDeGrupos
 {
@@ -9,28 +10,59 @@ namespace GeradorDeGrupos
         {
             Console.WriteLine("Bem vindo ao gerador de grupos aleatórios!");
             Console.Write("\nPrimeiro, digite o número de pessoas que participarão: ");
-            decimal qtdPessoas = decimal.Parse(Console.ReadLine());
+            string qtd_Pessoas = Console.ReadLine();
+            bool validQtd_Pessoas = decimal.TryParse(qtd_Pessoas, out decimal qtdPessoas);
+
+            while(validQtd_Pessoas == false)
+            {
+                Console.WriteLine("Só aceito números!\nDigite o número de pessoas que participarão: ");
+                qtd_Pessoas = Console.ReadLine();
+                validQtd_Pessoas = decimal.TryParse(qtd_Pessoas, out qtdPessoas);
+            }
 
             List<string> nomes = new();
+            Regex regex = new("[ ]{2,}");
 
             for (int i = 0; i < qtdPessoas; i++)
             {
                 Console.Write($"Digite o {i+1}º nome: ");
-                string incluirNome = Console.ReadLine();
+                string incluirNome = Console.ReadLine().Trim();
+                incluirNome = regex.Replace(incluirNome, " ");
+
+                while(string.IsNullOrWhiteSpace(incluirNome))
+                {
+                    Console.WriteLine("Não aceito espaço em branco ou valor nulo!");
+                    Console.WriteLine("Digite um nome valido!");
+                    Console.Write($"Digite o {i + 1}º nome: ");
+                    incluirNome = Console.ReadLine().Trim();
+                    incluirNome = regex.Replace(incluirNome, " ");
+                }
+
                 string validadorNome = nomes.Find(x => x == incluirNome);
                 while (validadorNome != null)
                 {
                     Console.WriteLine("Nome já digitado, inclua outro nome!");
+                    Console.WriteLine("Obs.: Caso existam nomes iguais, pode utilizar letra maiuscula para diferenciar");
                     Console.Write($"Digite o {i + 1}º nome: ");
-                    incluirNome = Console.ReadLine();
+                    incluirNome = Console.ReadLine().Trim();
+                    incluirNome = regex.Replace(incluirNome, " ");
                     validadorNome = nomes.Find(x => x == incluirNome);
                 }
                 nomes.Add(incluirNome);
             }
 
             Console.WriteLine("\nQuantos serão os grupos a serem formados? ");
-            int numGrupos = int.Parse(Console.ReadLine());
+            string num_Grupos = Console.ReadLine();
+            bool validNum_Grupos = int.TryParse(num_Grupos, out int numGrupos);
 
+            while (validNum_Grupos == false)
+            {
+                Console.WriteLine("Só aceito números!\nDigite o número de grupos a serem formados: ");
+                num_Grupos = Console.ReadLine();
+                validNum_Grupos = int.TryParse(num_Grupos, out numGrupos);
+            }
+
+            // Arredondamento para cima
             int numIntegrantes = (int)Math.Ceiling(qtdPessoas / numGrupos);
 
             Console.Clear();
